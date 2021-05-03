196d97542a4c770d5f39b88289eef3156d0ff40a
Elke Heymans <el.heymans@gmail.com>
2021-05-03 22:58:28 +0200

    Chapter 01: setup done, creating first component

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-primary-500">Hello Bold!</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#ebf8ff",
+          200: "#bee3f8",
+          //ordina color
+          300: "#E37C30",
+          400: "#63b3ed",
+          //ordina color
+          500: "#E7904B",
+          600: "#3182ce",
+          //ordina color
+          700: "#EAB285",
+          800: "#2c5282",
+          //ordina color
+          900: "#F7D1A7",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
