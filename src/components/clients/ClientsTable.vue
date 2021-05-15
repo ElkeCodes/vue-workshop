aac3e09e1a93abd0ec57ed29596e93bc927157ad
Elke Heymans <el.heymans@gmail.com>
2021-05-15 21:50:02 +0200

    Chapter 04: Added clients page contents, creating the CreateClient component

---
+++ src/components/clients/ClientsTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <table class="table-auto w-full">
+    <thead>
+      <tr class="bg-gray-200">
+        <th class="border px-4 py-2 text-left">Id</th>
+        <th class="border px-4 py-2 text-left">Name</th>
+        <th class="border px-4 py-2 text-left"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="client in clients">
+        <td class="border px-4 py-2">{{ client.id }}</td>
+        <td class="border px-4 py-2">
+          {{ client.firstName }} {{ client.lastName }}
+        </td>
+        <td class="border px-4 py-2">
+          Actions
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Client } from "../../models/Client";
+
+export default defineComponent({
+  name: "ClientsTable",
+  props: {
+    clients: Array as PropType<Client[]>,
+  },
+});
+</script>
--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -1,3 +1,36 @@
 <template>
-  <div>Clients</div>
+  <div class="pl-6 pr-6 divide-y divide-gray-400">
+    <div class="relative">
+      <h1 class="pt-6 pb-6 text-xl">Clients</h1>
+    </div>
+    <div class="pt-6">
+      <ClientsTable :clients="clients"></ClientsTable>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ClientsTable from "../components/clients/ClientsTable.vue";
+
+export default defineComponent({
+  components: {
+    ClientsTable,
+  },
+  data() {
+    return {
+      clients: [],
+    };
+  },
+  methods: {
+    loadClients() {
+      fetch("https://base-app-backend.herokuapp.com/clients")
+        .then((response) => response.json())
+        .then((clients) => (this.clients = clients));
+    },
+  },
+  created() {
+    this.loadClients();
+  },
+});
+</script>
