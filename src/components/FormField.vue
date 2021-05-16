330cf60cfd13ca4500c8a3ff369dc8fceaf74d06
Elke Heymans <el.heymans@gmail.com>
2021-05-16 11:38:18 +0200

    Chapter 06: Wrote FormField component, will add validation

---
+++ src/components/FormField.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="flex items-center mb-6">
+    <div class="w-1/4">
+      <label
+        class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        >{{ label }}</label
+      >
+    </div>
+    <div class="w-3/4">
+      <input
+        class="
+          bg-gray-200
+          appearance-none
+          border-2 border-gray-200
+          rounded
+          w-full
+          py-2
+          px-4
+          text-gray-700
+          leading-tight
+          focus:outline-none
+          focus:bg-white
+          focus:border-primary-500
+        "
+        type="text"
+        v-model="value"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+    },
+  },
+  emits: ["update:modelValue"],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+});
+</script>
--- src/components/clients/ClientForm.vue
+++ src/components/clients/ClientForm.vue
@@ -1,125 +1,11 @@
 <template>
   <form class="w-full" @submit.prevent>
-    <div class="flex items-center mb-6">
-      <div class="w-1/4">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          >First name</label
-        >
-      </div>
-      <div class="w-3/4">
-        <input
-          class="
-            bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full
-            py-2 px-4 text-gray-700 leading-tight focus:outline-none
-            focus:bg-white focus:border-primary-500
-          "
-          type="text"
-          name="firstName"
-          v-model="client.firstName"
-        />
-      </div>
-    </div>
-    <div class="flex items-center mb-6">
-      <div class="w-1/4">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          >Last name</label
-        >
-      </div>
-      <div class="w-3/4">
-        <input
-          class="
-            bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full
-            py-2 px-4 text-gray-700 leading-tight focus:outline-none
-            focus:bg-white focus:border-primary-500
-          "
-          type="text"
-          name="lastName"
-          v-model="client.lastName"
-        />
-      </div>
-    </div>
-    <div class="flex items-center mb-6">
-      <div class="w-1/4">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          >E-mail</label
-        >
-      </div>
-      <div class="w-3/4">
-        <input
-          class="
-            bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full
-            py-2 px-4 text-gray-700 leading-tight focus:outline-none
-            focus:bg-white focus:border-primary-500
-          "
-          type="text"
-          name="email"
-          v-model="client.email"
-        />
-      </div>
-    </div>
-    <div class="flex items-center mb-6">
-      <div class="w-1/4">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          >Birthday</label
-        >
-      </div>
-      <div class="w-3/4">
-        <input
-          class="
-            bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full
-            py-2 px-4 text-gray-700 leading-tight focus:outline-none
-            focus:bg-white focus:border-primary-500
-          "
-          type="text"
-          name="birthday"
-          v-model="client.birthday"
-        />
-      </div>
-    </div>
-    <div class="flex items-center mb-6">
-      <div class="w-1/4">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          >City</label
-        >
-      </div>
-      <div class="w-3/4">
-        <input
-          class="
-            bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full
-            py-2 px-4 text-gray-700 leading-tight focus:outline-none
-            focus:bg-white focus:border-primary-500
-          "
-          type="text"
-          name="city"
-          v-model="client.city"
-        />
-      </div>
-    </div>
-    <div class="flex items-center mb-6">
-      <div class="w-1/4">
-        <label
-          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-          >ZIP</label
-        >
-      </div>
-      <div class="w-3/4">
-        <input
-          class="
-            bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full
-            py-2 px-4 text-gray-700 leading-tight focus:outline-none
-            focus:bg-white focus:border-primary-500
-          "
-          type="text"
-          name="zip"
-          v-model="client.zip"
-        />
-      </div>
-    </div>
+    <form-field v-model="client.firstName" label="First name" />
+    <form-field v-model="client.lastName" label="Last name" />
+    <form-field v-model="client.email" label="E-mail" />
+    <form-field v-model="client.birthday" label="Birthday" />
+    <form-field v-model="client.city" label="City" />
+    <form-field v-model="client.zip" label="ZIP" />
     <div class="flex items-center">
       <div class="md:w-1/4"></div>
       <div class="md:w-3/4 flex">
@@ -127,8 +13,13 @@
           <button
             type="submit"
             class="
-              bg-primary-500 text-white font-bold
-              py-2 px-4 rounded hover:bg-primary-700
+              bg-primary-500
+              text-white
+              font-bold
+              py-2
+              px-4
+              rounded
+              hover:bg-primary-700
             "
             @click="onSubmit"
           >
@@ -138,8 +29,13 @@
         <button
           type="button"
           class="
-            bg-transparent hover:bg-primary-700 border border-primary-500
-            ml-4 py-2 px-4 rounded
+            bg-transparent
+            hover:bg-primary-700
+            border border-primary-500
+            ml-4
+            py-2
+            px-4
+            rounded
           "
           @click="onCancel"
         >
@@ -153,6 +49,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { Client } from "../../models/Client";
+import FormField from "../../components/FormField.vue";
 
 export default defineComponent({
   props: {
@@ -169,6 +66,9 @@ export default defineComponent({
     onCancel() {
       this.$emit("cancel");
     },
+  },
+  components: {
+    FormField,
   },
 });
 </script>
